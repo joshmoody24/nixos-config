4532ae14820beaf59df40a1b69e4903b6cccb246
Josh Moody <joshmoody24@gmail.com>
2025-12-05 11:12:19 -0700

    paredit wrapping

--- shared/dotfiles/nvim/lua/josh/plugins/paredit.lua
+++ shared/dotfiles/nvim/lua/josh/plugins/paredit.lua
@@ -1,6 +1,20 @@
 return {
   "julienvincent/nvim-paredit",
   config = function()
-    require("nvim-paredit").setup()
+    local paredit = require("nvim-paredit")
+    paredit.setup()
+
+    -- Wrapping
+    vim.keymap.set("n", "<localleader>w", function()
+      paredit.api.wrap_element_under_cursor("(", ")")
+    end, { desc = "Wrap element" })
+
+    vim.keymap.set("n", "<localleader>W", function()
+      paredit.api.wrap_enclosing_form_under_cursor("(", ")")
+    end, { desc = "Wrap enclosing form" })
+
+    vim.keymap.set("n", "<localleader>u", function()
+      paredit.api.unwrap_form_under_cursor()
+    end, { desc = "Unwrap form" })
   end,
 }
