98bf6024d1b259a0efbdad5edc0caed1ec59e578
Josh Moody <joshmoody24@gmail.com>
2026-01-08 15:17:05 -0700

    Redo alias command

--- hosts/framework/home.nix
+++ hosts/framework/home.nix
@@ -27,6 +27,7 @@
     ".config/kitty/redo.session".source = ./redo.session;
     ".bashrc.local".text = ''
     alias bazel="distrobox enter redo -- bazelisk"
+    alias redoservices="distrobox enter redo -- bazelisk run services redo-cockroachdb redo-valkey temporal-server temporal-worker redo-notice-server redo-api-server redo-merchant-server redo-shopify-server redo-public-server redo-admin-server redo-merchant-app-vite redo-admin-app-vite redo-return-app-vite redo-storefront redo-shopify-extension"
     '';
     ".local/bin/open-browser-from-container" = {
       source = ./open-browser-from-container.sh;
