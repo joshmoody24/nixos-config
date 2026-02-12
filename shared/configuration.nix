eb4e4edda4f13c9cc29ec7125e8477ecfcac3016
Josh Moody <joshmoody24@gmail.com>
2026-02-12 14:29:44 -0700

    Experimental keyd typing enhancements

--- shared/configuration.nix
+++ shared/configuration.nix
@@ -88,6 +88,69 @@
     nix-direnv.enable = true;
   };
 
+  # Keyboard remapping with keyd
+  services.keyd = {
+    enable = true;
+    keyboards.default = {
+      ids = [ "*" ];
+      settings = {
+        main = {
+          capslock = "esc";
+          # Shift+space chord activates pull-down symbols (momentary)
+          "leftshift+space" = "layer(symbols)";
+          "rightshift+space" = "layer(symbols)";
+          # Bottom row mods (tap = letter, hold = modifier + hjkl nav)
+          z = "overloadt(ctrl_nav, z, 200)";
+          x = "overloadt(alt_nav, x, 200)";
+          # x+c toggles half-QWERTY mode (changes space behavior)
+          "q+w" = "toggle(halfqwerty_mode)";
+        };
+
+        # Pull-down number/symbol layer
+        "symbols" = {
+          capslock = "capslock";
+          a = "!"; s = "@"; d = "#"; f = "$"; g = "%";
+          h = "^"; j = "&"; k = "*"; l = "("; ";" = ")";
+          apostrophe = "_";
+          q = "1"; w = "2"; e = "3"; r = "4"; t = "5";
+          y = "6"; u = "7"; i = "8"; o = "9"; p = "0";
+          leftbrace = "-"; rightbrace = "=";
+          z = "`"; x = "~"; c = "-"; v = "="; b = "[";
+          n = "]"; m = "{"; "," = "}"; "." = "\\"; "/" = "|";
+        };
+
+        # Ctrl + nav (hold z)
+        "ctrl_nav:C" = {
+          h = "C-left"; j = "C-down"; k = "C-up"; l = "C-right";
+        };
+
+        # Alt + nav (hold x)
+        "alt_nav:A" = {
+          h = "A-left"; j = "A-down"; k = "A-up"; l = "A-right";
+        };
+
+        # Half-QWERTY mode: hold space = mirror, tap space = space (key-up)
+        "halfqwerty_mode" = {
+          "q+w" = "toggle(halfqwerty_mode)";
+          space = "overload(mirror, space)";
+          capslock = "enter";
+          "`" = "backspace";
+        };
+
+        # Mirror layer (activated by holding space in halfqwerty_mode)
+        "mirror" = {
+          "`" = "backspace";
+          q = "p"; w = "o"; e = "i"; r = "u"; t = "y";
+          a = ";"; s = "l"; d = "k"; f = "j"; g = "h";
+          z = "/"; x = "."; c = ","; v = "n"; b = "m";
+          p = "q"; o = "w"; i = "e"; u = "r"; y = "t";
+          ";" = "a"; l = "s"; k = "d"; j = "f"; h = "g";
+          "/" = "z"; "." = "x"; "," = "c"; n = "v"; m = "b";
+        };
+      };
+    };
+  };
+
   # Enable the OpenSSH daemon.
   services.openssh.enable = true;
 }
