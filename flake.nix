8f5f10c7024cf96b5053c0b0de37ac823f8256db
Josh Moody <joshmoody24@gmail.com>
2026-02-23 12:01:35 -0700

    thinkpad + framework home manager only

--- flake.nix
+++ flake.nix
@@ -1,55 +1,50 @@
 {
-  description = "Josh's multi-machine NixOS config";
+  description = "Josh's multi-machine config (NixOS + Ubuntu)";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     home-manager = {
       url = "github:nix-community/home-manager";
-      # The `follows` keyword in inputs is used for inheritance.
-      # Here, `inputs.nixpkgs` of home-manager is kept consistent with
-      # the `inputs.nixpkgs` of the current flake,
-      # to avoid problems caused by different versions of nixpkgs.
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { self, nixpkgs, home-manager, ... }@inputs: {
+  outputs = { self, nixpkgs, home-manager, ... }@inputs:
+  let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs {
+      inherit system;
+      config.allowUnfree = true;
+    };
+  in {
+    # NixOS
     nixosConfigurations.unit = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [
         ./hosts/unit/configuration.nix
         home-manager.nixosModules.home-manager
-	{
-	  home-manager.useGlobalPkgs = true;
-	  home-manager.useUserPackages = true;
-	  home-manager.users.josh = import ./hosts/unit/home.nix;
-	}
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.users.josh = import ./hosts/unit/home.nix;
+        }
       ];
     };
-    nixosConfigurations."joshm-thinkpad" = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
+
+    # Standalone home-manager (Ubuntu)
+    homeConfigurations."josh@joshm-thinkpad" = home-manager.lib.homeManagerConfiguration {
+      inherit pkgs;
       modules = [
-        ./hosts/thinkpad/configuration.nix
-        home-manager.nixosModules.home-manager
-	{
-	  home-manager.useGlobalPkgs = true;
-	  home-manager.useUserPackages = true;
-	  home-manager.users.josh = import ./hosts/thinkpad/home.nix;
-          home-manager.backupFileExtension = "backup";
-	}
+        ./hosts/thinkpad/home.nix
+        { home.backupFileExtension = "backup"; }
       ];
     };
-    nixosConfigurations."joshm-framework" = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
+
+    homeConfigurations."josh@joshm-framework" = home-manager.lib.homeManagerConfiguration {
+      inherit pkgs;
       modules = [
-        ./hosts/framework/configuration.nix
-        home-manager.nixosModules.home-manager
-	{
-	  home-manager.useGlobalPkgs = true;
-	  home-manager.useUserPackages = true;
-	  home-manager.users.josh = import ./hosts/framework/home.nix;
-          home-manager.backupFileExtension = "backup";
-	}
+        ./hosts/framework/home.nix
+        { home.backupFileExtension = "backup"; }
       ];
     };
   };
